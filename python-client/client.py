e112e63cb1b45eda002c2126d65fafd10375c317
Thomas Eiszler <teiszler@andrew.cmu.edu>
2021-11-12 12:27:22 -0500

    We now properly produce the extras (location and model) for the client when using the OpenCV adapter (aka the camera as input).

--- python-client/client.py
+++ python-client/client.py
@@ -20,23 +20,33 @@ from gabriel_client.websocket_client import WebsocketClient
 from gabriel_client.opencv_adapter import OpencvAdapter
 from gabriel_protocol import gabriel_pb2
 import logging
-import zmq
 from zmq_adapter import ZmqAdapter
+import openscout_pb2
+import uuid
+import geocoder
 
+UUID = str(uuid.uuid4())
 WEBSOCKET_PORT = 9099
 DEFAULT_SOURCE_NAME = 'openscout'
 
 logger = logging.getLogger(__name__)
+logging.getLogger("geocoder").setLevel(logging.WARNING)
 
 def preprocess(frame):
     return frame
 
 def produce_extras():
-    pass
+    extras = openscout_pb2.Extras()
+    extras.client_id = UUID
+    g = geocoder.ip('me')
+    extras.location.latitude = g.latlng[0]
+    extras.location.longitude = g.latlng[1]
+    extras.model = 'coco'
+    return extras
 
 def local_consumer(result_wrapper):
     if len(result_wrapper.results) != 1:
-        logger.error('Got %d results from server',
+        logger.debug('Got %d results from server',
                         len(result_wrapper.results))
         return
 
@@ -48,16 +58,16 @@ def local_consumer(result_wrapper):
     logger.info(result.payload.decode('utf-8'))
 
 def main():
-    logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--server', default='openscout-demo.cmusatyalab.org',
         help='Specify address of OpenScout server [default: openscout-demo.cmusatyalab.org')
     parser.add_argument('-p', '--port', default='9099', help='Specify websocket port [default: 9099]')
     parser.add_argument('-c', '--camera', action='store_true', help='Use cv2.VideoCapture(0) adapter instead of ZmqAdapter')
     parser.add_argument('-d', '--display', action='store_true', help='Optionally display the frames received by the ZmqAdapter using cv2.imshow')
+    parser.add_argument('-l', '--loglevel', default='INFO', help='Set the log level')
     
     args = parser.parse_args()
-
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=args.loglevel)
     if args.camera:
         capture = cv2.VideoCapture(0)
         adapter = OpencvAdapter(
